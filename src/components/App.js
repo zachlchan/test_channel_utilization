72df54a587c5369fc07873e119b99e0aeb448e9d
Zach Chan <zacharyleechan@gmail.com>
2021-03-05 12:25:18 -0800

    implemented getChannels method to retrieve data from the mock API

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,19 @@
 import '../mockAPI'; // Do not remove or modify this line
 import React from 'react';
 // Add the following import to any file you use async/await in
-// import 'babel-polyfill';
+import 'babel-polyfill';
 
 const App = () => {
+    const getChannels = async () => {
+        const response = await fetch('tests');
+        const jsonResponse = await response.json();
+        return jsonResponse;
+    };
+
     return (
         <div>
             <h1>Hello, here's the app!</h1>
+            <button type="button" onClick={getChannels}>Get Data</button>
         </div>
     );
 };
