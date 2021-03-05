8fae087741c28f1d22df82b45ff88d6de8b92de7
Zach Chan <zacharyleechan@gmail.com>
2021-03-05 12:56:37 -0800

    implemented getTests method to retrieve data from the mock API and moved fetch to getData helper

--- src/components/App.js
+++ src/components/App.js
@@ -1,21 +1,42 @@
 import '../mockAPI'; // Do not remove or modify this line
 import React from 'react';
+import styled from 'styled-components';
 // Add the following import to any file you use async/await in
 import 'babel-polyfill';
 
 const App = () => {
+    const getData = async (endpoint) => {
+        const response = await fetch(endpoint);
+        return response.json();
+    };
+
     const getChannels = async () => {
-        const response = await fetch('tests');
-        const jsonResponse = await response.json();
-        return jsonResponse;
+        const data = await (getData('channels'));
+        return data;
+    };
+
+    const getTests = async () => {
+        const data = await (getData('tests'));
+        return data;
     };
 
     return (
-        <div>
-            <h1>Hello, here's the app!</h1>
-            <button type="button" onClick={getChannels}>Get Data</button>
-        </div>
+        <StyledWrapper>
+            <h1>Channel Utilization</h1>
+            <button type="button" onClick={getChannels}>
+                Get Channel Data
+            </button>
+            <button type="button" onClick={getTests}>
+                Get Test Data
+            </button>
+        </StyledWrapper>
     );
 };
+
+const StyledWrapper = styled.div`
+    & h1 {
+        font-family: 'Source Sans Pro',Helvetica,Arial,Lucida,sans-serif;
+    }
+`;
 
 export default App;
