232dc8c0dee436692d2fb402726b6837b6da64e9
Zach Chan <zacharyleechan@gmail.com>
2021-03-05 11:58:12 -0800

    initialized  repo

---
+++ __tests__/testOne.js
@@ -0,0 +1,57 @@
+/* eslint-disable no-undef */
+const expectedChannelUtilizations = [
+    {
+        date: '2019-11-04T00:00:00.000Z',
+        utilizationPercentage: 14,
+    },
+    {
+        date: '2019-11-05T00:00:00.000Z',
+        utilizationPercentage: 86,
+    },
+    {
+        date: '2019-11-06T00:00:00.000Z',
+        utilizationPercentage: 71,
+    },
+    {
+        date: '2019-11-07T00:00:00.000Z',
+        utilizationPercentage: 43,
+    },
+    {
+        date: '2019-11-08T00:00:00.000Z',
+        utilizationPercentage: 57,
+    },
+    {
+        date: '2019-11-09T00:00:00.000Z',
+        utilizationPercentage: 29,
+    },
+    {
+        date: '2019-11-10T00:00:00.000Z',
+        utilizationPercentage: 29,
+    },
+    {
+        date: '2019-11-11T00:00:00.000Z',
+        utilizationPercentage: 57,
+    },
+    {
+        date: '2019-11-12T00:00:00.000Z',
+        utilizationPercentage: 57,
+    },
+    {
+        date: '2019-11-13T00:00:00.000Z',
+        utilizationPercentage: 57,
+    },
+    {
+        date: '2019-11-14T00:00:00.000Z',
+        utilizationPercentage: 43,
+    },
+    {
+        date: '2019-11-15T00:00:00.000Z',
+        utilizationPercentage: 43,
+    },
+];
+
+describe('Your test suite', () => {
+    it('should test if your work is accurate.', () => {
+        expect().toEqual(expectedChannelUtilizations);
+    });
+});
---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import '../mockAPI'; // Do not remove or modify this line
+import React from 'react';
+// Add the following import to any file you use async/await in
+// import 'babel-polyfill';
+
+const App = () => {
+    return (
+        <div>
+            <h1>Hello, here's the app!</h1>
+        </div>
+    );
+};
+
+export default App;
